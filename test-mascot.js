c15009264f3670d312832b0e08c3832d5ba04c0f
Tvinck <112720884+Tvinck@users.noreply.github.com>
2026-07-01 19:12:58 +0300

    chore: remove test scripts

--- test-mascot.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-https.get('https://connect-4va6.vercel.app/mascot.png', (res) => {
-  console.log('Status Code:', res.statusCode);
-  console.log('Content-Type:', res.headers['content-type']);
-  console.log('Content-Length:', res.headers['content-length']);
-}).on('error', (e) => {
-  console.error(e);
-});
